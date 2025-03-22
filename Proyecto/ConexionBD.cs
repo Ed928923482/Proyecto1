3790d9fb97d9043adbd1fe6d13b2c883f61210bc
user <user@DESKTOP-G3BNU6U>
2025-03-21 20:00:05 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto/ConexionBD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Proyecto
+{
+    class ConexionBD
+    {
+        public SqlConnection conectar = new SqlConnection("Data Source=USUARIO\\SQLEXPRESS;Initial Catalog=Examen;Integrated Security=True");
+
+        public void Abrir()
+        {
+            try
+            {
+                if (conectar.State == System.Data.ConnectionState.Closed)
+                {
+                    conectar.Open();
+                    Console.WriteLine(" Conexión abierta");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" Error al abrir base de datos: " + ex.Message);
+            }
+        }
+
+        public void Cerrar()
+        {
+            try
+            {
+                if (conectar.State == System.Data.ConnectionState.Open)
+                {
+                    conectar.Close();
+                    Console.WriteLine(" Conexión cerrada");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" Error al cerrar base de datos: " + ex.Message);
+            }
+        }
+    }
+}
